99b450d39978ec4e7bdc4ddb63eb58710171bf36
Dylan Tettarasar <dtettarasar@gmail.com>
2025-08-26 09:41:58 +0000

    building the view entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -12,3 +12,14 @@ def create(request):
         "entries": util.list_entries()
     })
 
+def view_entry(request, entry_title):
+
+    print("entry_title: ")
+    print(entry_title)
+
+    entry_data = util.get_entry(entry_title)
+    print(entry_data)
+
+    return render(request, "encyclopedia/view_entry.html", {
+        "entries": util.list_entries()
+    })
